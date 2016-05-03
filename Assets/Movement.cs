fd0a437df5d0924ad76458446a0c9f7873edba19
zhuoyikang <zhuoyikang@gmail.com>
2016-05-03 22:36:16 +0800

    first

---
+++ Assets/Movement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float speed = 5;
+	public float angularSpeed = 10;
+	public Rigidbody _rigidBody ; 
+
+	// Use this for initialization
+	void Start () {
+		_rigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+		float h = Input.GetAxis ("Vertical");
+		_rigidBody.velocity = transform.forward * h * speed;
+
+		float v = Input.GetAxis("Horizontal");
+		_rigidBody.angularVelocity = transform.up * v * angularSpeed;
+	}
+}
