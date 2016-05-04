9ad557db56db2adc12c9972914c5e233af0012ff
zhuoyikang <zhuoyikang@gmail.com>
2016-05-04 21:09:19 +0800

    摄像机跟随

---
+++ Assets/Follow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+	public Transform player1;
+	public Transform player2;
+
+	private Vector3 offset;
+	private Camera camera;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - (player1.position + player2.position) / 2;
+		camera = this.GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = (player1.position + player2.position) / 2 + offset;
+		float distance = Vector3.Distance (player1.position, player2.position);
+		float size = distance * 1;
+
+		camera.orthographicSize = size;
+	}
+}
